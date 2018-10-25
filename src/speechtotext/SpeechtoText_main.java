372b668e9ef85915afd40a938bcf09844bf2f20c
1618042 <1618042@cs16042>
2018-10-25 14:50:36 +0900

    track06

--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -17,7 +17,7 @@ public class SpeechtoText_main {
 		
 		
 		SpeechToText service = new SpeechToText();
-	    service.setUsernameAndPassword("8921e281-73a0-4129-8c51-2239bfa65ec4", "KckSezhMYAuB");
+	    service.setUsernameAndPassword("1618042", "1618042");
 
 	    File audio = new File("audio/track06.wav");
 	    RecognizeOptions options = null;
